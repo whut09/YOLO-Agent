64b6b2f0fea666e772bafc9c7095452974fa887b
chenbo <609345256@qq.com>
2026-07-29 11:17:18 +0800

    Export isolated SAHI inference APIs

--- yolo_agent/components/adapters/inference/__init__.py
+++ yolo_agent/components/adapters/inference/__init__.py
@@ -1,5 +1,17 @@
 """Inference-only component adapters."""
 
+from yolo_agent.components.adapters.inference.artifacts import (
+    SlicingArtifactPaths,
+    write_slicing_artifacts,
+)
+from yolo_agent.components.adapters.inference.sahi_backend import (
+    SahiSlicingBackend,
+    SlicingImage,
+)
+from yolo_agent.components.adapters.inference.sliced_coco import (
+    SlicedCocoMetrics,
+    evaluate_sliced_coco,
+)
 from yolo_agent.components.adapters.inference.slicing import (
     SlicingInferenceAdapter,
     SlicingInferenceConfig,
@@ -9,9 +21,15 @@ from yolo_agent.components.adapters.inference.slicing import (
 )
 
 __all__ = [
+    "SahiSlicingBackend",
+    "SlicedCocoMetrics",
+    "SlicingArtifactPaths",
     "SlicingInferenceAdapter",
     "SlicingInferenceConfig",
     "SlicingInferenceMetrics",
     "SlicingInferenceProtocol",
     "SlicingInferenceResult",
+    "SlicingImage",
+    "evaluate_sliced_coco",
+    "write_slicing_artifacts",
 ]
