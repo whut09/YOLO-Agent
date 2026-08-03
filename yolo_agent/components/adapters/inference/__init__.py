5c31ca88753db824cfe49d70adbb4db944e8420b
chenbo <609345256@qq.com>
2026-08-03 13:57:58 +0800

    Export isolated inference policy adapters

--- yolo_agent/components/adapters/inference/__init__.py
+++ yolo_agent/components/adapters/inference/__init__.py
@@ -4,6 +4,31 @@ from yolo_agent.components.adapters.inference.artifacts import (
     SlicingArtifactPaths,
     write_slicing_artifacts,
 )
+from yolo_agent.components.adapters.inference.backend import (
+    BackendResult,
+    InferenceImage,
+    UltralyticsInferenceBackend,
+)
+from yolo_agent.components.adapters.inference.plugin import (
+    ClassAwareThresholdInferenceAdapter,
+    ConfidenceCalibrationInferenceAdapter,
+    InferencePolicyPlugin,
+    MergePolicyInferenceAdapter,
+    TestTimeAugmentationAdapter,
+    TiledMultiScaleInferenceAdapter,
+)
+from yolo_agent.components.adapters.inference.policy import (
+    InferencePolicyConfig,
+    InferencePolicyMetrics,
+    InferencePolicyProtocol,
+    InferencePolicyResult,
+    InferenceResourceMetrics,
+    protocol_from_policy,
+)
+from yolo_agent.components.adapters.inference.policy_artifacts import (
+    InferencePolicyArtifactPaths,
+    write_inference_policy_artifacts,
+)
 from yolo_agent.components.adapters.inference.sahi_backend import (
     SahiSlicingBackend,
     SlicingImage,
@@ -21,6 +46,18 @@ from yolo_agent.components.adapters.inference.slicing import (
 )
 
 __all__ = [
+    "BackendResult",
+    "ClassAwareThresholdInferenceAdapter",
+    "ConfidenceCalibrationInferenceAdapter",
+    "InferenceImage",
+    "InferencePolicyArtifactPaths",
+    "InferencePolicyConfig",
+    "InferencePolicyMetrics",
+    "InferencePolicyPlugin",
+    "InferencePolicyProtocol",
+    "InferencePolicyResult",
+    "InferenceResourceMetrics",
+    "MergePolicyInferenceAdapter",
     "SahiSlicingBackend",
     "SlicedCocoMetrics",
     "SlicingArtifactPaths",
@@ -30,6 +67,11 @@ __all__ = [
     "SlicingInferenceProtocol",
     "SlicingInferenceResult",
     "SlicingImage",
+    "TestTimeAugmentationAdapter",
+    "TiledMultiScaleInferenceAdapter",
+    "UltralyticsInferenceBackend",
     "evaluate_sliced_coco",
+    "protocol_from_policy",
+    "write_inference_policy_artifacts",
     "write_slicing_artifacts",
 ]
