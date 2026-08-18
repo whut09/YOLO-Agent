63c7be4903bd57d53ccbf7c15f5dbfc033a42111
chenbo <609345256@qq.com>
2026-08-18 15:05:25 +0800

    Export DistillationMethodRegistry from the adapter package

--- yolo_agent/components/adapters/distillation/__init__.py
+++ yolo_agent/components/adapters/distillation/__init__.py
@@ -1,8 +1,19 @@
 """Distillation component adapters."""
 
+from yolo_agent.components.adapters.distillation.method_registry import (
+    DistillationMethodRegistry,
+    DistillationTeacherMissingError,
+    default_distillation_method_registry,
+)
 from yolo_agent.components.adapters.distillation.yolo26_distillation import (
     YOLO26DistillationAdapter,
     YOLO26DistillationRuntimePlugin,
 )
 
-__all__ = ["YOLO26DistillationAdapter", "YOLO26DistillationRuntimePlugin"]
+__all__ = [
+    "DistillationMethodRegistry",
+    "DistillationTeacherMissingError",
+    "YOLO26DistillationAdapter",
+    "YOLO26DistillationRuntimePlugin",
+    "default_distillation_method_registry",
+]
