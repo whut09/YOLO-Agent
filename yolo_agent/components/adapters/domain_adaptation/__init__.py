9de9d322a00b4c35ead6beb62b7ccefaee5503f1
chenbo <609345256@qq.com>
2026-08-18 15:24:22 +0800

    Export domain adaptation branch adapters and registry

--- yolo_agent/components/adapters/domain_adaptation/__init__.py
+++ yolo_agent/components/adapters/domain_adaptation/__init__.py
@@ -1,11 +1,23 @@
 """Runtime adapters for explicitly supervised domain adaptation batches."""
 
+from yolo_agent.components.adapters.domain_adaptation.branch_runtime import (
+    DomainAdaptationBranchAdapter,
+    DomainAdaptationBranchPlugin,
+)
+from yolo_agent.components.adapters.domain_adaptation.branches import (
+    DomainAdaptationMethodRegistry,
+    default_domain_adaptation_registry,
+)
 from yolo_agent.components.adapters.domain_adaptation.feature_alignment import (
     DomainFeatureAlignmentAdapter,
     DomainFeatureAlignmentRuntimePlugin,
 )
 
 __all__ = [
+    "DomainAdaptationBranchAdapter",
+    "DomainAdaptationBranchPlugin",
+    "DomainAdaptationMethodRegistry",
     "DomainFeatureAlignmentAdapter",
     "DomainFeatureAlignmentRuntimePlugin",
+    "default_domain_adaptation_registry",
 ]
