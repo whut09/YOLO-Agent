0ca3c40c36275b5781988db14d9c4d11541ae75b
chenbo <609345256@qq.com>
2026-09-08 09:49:56 +0800

    Expose Mutual Supervision adapter exports

--- yolo_agent/components/adapters/losses/__init__.py
+++ yolo_agent/components/adapters/losses/__init__.py
@@ -4,5 +4,14 @@ from yolo_agent.components.adapters.losses.quality_alignment import (
     QualityAlignmentAuxiliaryLossAdapter,
     QualityAlignmentRuntimePlugin,
 )
+from yolo_agent.components.adapters.losses.mutual_supervision import (
+    MutualSupervisionAdapter,
+    MutualSupervisionRuntimePlugin,
+)
 
-__all__ = ["QualityAlignmentAuxiliaryLossAdapter", "QualityAlignmentRuntimePlugin"]
+__all__ = [
+    "MutualSupervisionAdapter",
+    "MutualSupervisionRuntimePlugin",
+    "QualityAlignmentAuxiliaryLossAdapter",
+    "QualityAlignmentRuntimePlugin",
+]
